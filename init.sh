62b9f119cd7ae9b97cf687b390b54eef87f4c78b
Denis727 <>
2018-10-16 20:46:25 +0300

    Add Django application

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
 sudo /etc/init.d/nginx restart
-sudo gunicorn -c /home/box/web/etc/hello.py hello:app &
-#sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/test
-#sudo /etc/init.d/gunicorn restart
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:app
+sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
