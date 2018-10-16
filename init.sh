19c5daee6b8d7d7d177ed1324516772984455900
Denis727 <>
2018-10-16 21:44:50 +0300

    Fix init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
-sudo ln -sf /home/root/web/etc/nginx.conf /etc/nginx/nginx.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
 sudo /etc/init.d/nginx restart
-sudo gunicorn -c /home/root/web/etc/gunicorn.conf hello:app &
-sudo gunicorn -c /home/root/web/etc/gunicorn-django.conf ask.wsgi:application &
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:app &
+sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application &
